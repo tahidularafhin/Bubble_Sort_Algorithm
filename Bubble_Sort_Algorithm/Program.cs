6894fd1e1e5de4eb04185c58e3501350f8d36426
tahidularafhin <48841466+tahidularafhin@users.noreply.github.com>
2022-03-10 23:01:00 +0100

    first commit

---
+++ Bubble_Sort_Algorithm/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Bubble_Sort_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 89, -1, 0, 4, 2, 3 };
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int flag = 0;
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                        flag = 1;
+                    }
+
+                }
+
+                if (flag == 0)
+                {
+                    break;
+                }
+
+
+            }
+
+
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
